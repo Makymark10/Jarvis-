b05e86a1b5f8802b14be7aa5fdaed63b455f8be3
makymark <91844322+Makymark10@users.noreply.github.com>
2022-03-23 23:52:44 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const microphoneButton = document.getElementById('microphoneButton');
 
 microphoneButton.addEventListener('click',function(){
     window.open(
-        './mic/index1.html',
+        'https://makymark10.github.io/mic/',
         "",
         "width=280px,height=280px"
     );
